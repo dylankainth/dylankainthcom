bfd66aedbb89094e0bc294c805c813617f8b2eb3
Dylan K <50585244+dylankainth@users.noreply.github.com>
2022-09-14 20:26:06 +0000

    added CV page

---
+++ pages/cv.vue
@@ -0,0 +1,32 @@
+<template>
+<div>
+<button class="btn btn-outline-light" @click="downloadCV">Download</button>
+</div>
+</template>
+
+
+<script>
+export default {
+    mounted() {
+        window.location = "https://docs.google.com/document/d/1HqRWw8HQaI-LCIO5_st4aAvzexoVRe3nKUFekwO_tPA/export?format=pdf"
+    },
+    methods : {
+        downloadCV() {
+            window.location = "https://docs.google.com/document/d/1HqRWw8HQaI-LCIO5_st4aAvzexoVRe3nKUFekwO_tPA/export?format=pdf"
+        }
+    }
+}
+</script>
+
+<style scoped>
+.cvparent {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+}
+.cvframe {
+    width: 100%;
+    height: 100%;
+}
+</style>
