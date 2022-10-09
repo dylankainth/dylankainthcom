fdbd168defb40f0815fc9336213149882342598a
Dylan K <50585244+dylankainth@users.noreply.github.com>
2022-10-09 12:53:24 +0000

    17

---
+++ pages/17.vue
@@ -0,0 +1,11 @@
+<template>
+<div>
+    <code style="font-size:2rem; color:white" >if (person.name == "Dylan Kainth") && (date==person.bday) {  <br>
+               &nbsp console.log("It's my birthday!") <br>
+    } 
+    </code>
+    <br>
+    <br>
+    <iframe src="https://giphy.com/embed/66dLOWrLqrgxWHSeVR" width="480" height="400" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/theoffice-the-office-tv-lecture-circuit-part-1-66dLOWrLqrgxWHSeVR">via GIPHY</a></p>
+</div>
+</template>
