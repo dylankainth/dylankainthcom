21ccb605da7cbe2232cc94bed85a7524a0aef09f
Dylan K <50585244+dylankainth@users.noreply.github.com>
2024-12-24 17:22:17 +0000

    fixed tag routing

--- pages/tag/_tag.vue
+++ pages/tag/_tag.vue
@@ -15,11 +15,8 @@
       </div>
     </div>
 
-    <div
-      v-if="$route.params.tag == 'Team Enigma'"
-      class="p-5 mb-4 rounded-3 shadow"
-      style="background-color: #3c6967; color: white; font-family: Poppins"
-    >
+    <div v-if="$route.params.tag == 'Team Enigma'" class="p-5 mb-4 rounded-3 shadow"
+      style="background-color: #3c6967; color: white; font-family: Poppins">
       <div class="container-fluid py-5">
         <div class="row align-items-start">
           <div class="col my-auto">
@@ -27,10 +24,7 @@
             <p>Team Enigma Projects are available on their website.</p>
           </div>
           <div class="col my-auto">
-            <div
-              class="h-100 p-5 rounded-3 shadow d-flex flex-column"
-              style="background-color:#2F5452"
-            >
+            <div class="h-100 p-5 rounded-3 shadow d-flex flex-column" style="background-color:#2F5452">
               <h1>Visit TE.</h1>
               <a type="button" class="btn btn-outline-light btn-lg" href="https://te.harrowschool.io/">Let's Go</a>
 
@@ -40,23 +34,20 @@
       </div>
     </div>
 
-    <div
-      v-if="$route.params.tag == 'RIE'"
-      class="p-5 mb-4 rounded-3 shadow"
-      style="background-color: #FFFFFF; color: #000000; font-family: Poppins"
-    >
+    <div v-if="$route.params.tag == 'RIE'" class="p-5 mb-4 rounded-3 shadow"
+      style="background-color: #FFFFFF; color: #000000; font-family: Poppins">
       <div class="container-fluid py-5">
         <div class="row my-4">
-          <div class="col">       
-            <center>  
-            <img src="~assets/images/kcllogo.png" width="auto" height="150px">
-          </center> 
+          <div class="col">
+            <center>
+              <img src="~assets/images/kcllogo.png" width="auto" height="150px">
+            </center>
           </div>
-            
-          <div class="col">            
-            <center>  
-            <img src="~assets/images/rielogo.png" width="auto" height="150px">
-          </center> 
+
+          <div class="col">
+            <center>
+              <img src="~assets/images/rielogo.png" width="auto" height="150px">
+            </center>
           </div>
         </div>
         <div class="row align-items-start">
@@ -65,12 +56,10 @@
             <h4 class="kclsecondaryfont">Projects are available on their Instagram.</h4>
           </div>
           <div class="col my-auto">
-            <div
-              class="h-100 p-5 rounded-3 shadow d-flex flex-column"
-              style="background-color:#E2231A"
-            >
+            <div class="h-100 p-5 rounded-3 shadow d-flex flex-column" style="background-color:#E2231A">
               <h1 style="color: #FFFFFF" class="kclsecondaryfont">Visit RIE on KCLSU.</h1>
-              <a type="button" class="btn btn-outline-light btn-lg kclprimaryfont" href="https://www.kclsu.org/groups/activities/join/rie/">Let's Go</a>
+              <a type="button" class="btn btn-outline-light btn-lg kclprimaryfont"
+                href="https://www.kclsu.org/groups/activities/join/rie/">Let's Go</a>
 
             </div>
           </div>
@@ -78,11 +67,8 @@
       </div>
     </div>
 
-    <div
-      v-if="$route.params.tag == 'School'"
-      class="p-5 mb-4 rounded-3 shadow"
-      style="background-color: #7a7d7d; color: white; font-family: Poppins"
-    >
+    <div v-if="$route.params.tag == 'School'" class="p-5 mb-4 rounded-3 shadow"
+      style="background-color: #7a7d7d; color: white; font-family: Poppins">
       <div class="container-fluid py-5">
         <div class="row align-items-start">
           <div class="col my-auto">
@@ -90,10 +76,7 @@
             <p>Some of these projects may be on the school Computer Science Society website.</p>
           </div>
           <div class="col my-auto">
-            <div
-              class="h-100 p-5 rounded-3 shadow d-flex flex-column"
-              style="background-color:#393939"
-            >
+            <div class="h-100 p-5 rounded-3 shadow d-flex flex-column" style="background-color:#393939">
               <h1>Visit CSS website</h1>
               <a type="button" class="btn btn-outline-light btn-lg" href="https://css.harrowschool.io/">Let's Go</a>
 
@@ -104,15 +87,8 @@
     </div>
 
 
-    <div
-      class="row row-cols-1 row-cols-lg-3 align-items-stretch g-4 py-5"
-      v-if="filtered"
-    >
-      <div
-        class="col"
-        v-for="(project, id) in filteredprojects"
-        v-bind:key="id"
-      >
+    <div class="row row-cols-1 row-cols-lg-3 align-items-stretch g-4 py-5" v-if="filtered">
+      <div class="col" v-for="(project, id) in filteredprojects" v-bind:key="id">
         <ProjectCard :projectdata="project" :projectid="id" />
       </div>
     </div>
@@ -128,7 +104,10 @@ export default {
   },
   mounted() {
     for (let x in this.$store.state.projects) {
-      if (this.$store.state.projects[x].tags.includes(this.$route.params.tag)) {
+      var tags = this.$store.state.projects[x].tags
+      var referencetag = this.$route.params.tag
+
+      if (tags.some(x => x.toLowerCase() == referencetag.toLowerCase()) == true) {
         this.filteredprojects[x] = this.$store.state.projects[x]
       }
     }
@@ -155,8 +134,8 @@ export default {
 }
 
 @font-face {
-    font-family: 'Kings Caslon Display Italic';
-    src: url('~assets/fonts/KingsCaslonDisplayItalic.ttf') format('truetype');
+  font-family: 'Kings Caslon Display Italic';
+  src: url('~assets/fonts/KingsCaslonDisplayItalic.ttf') format('truetype');
 }
 
 @font-face {
