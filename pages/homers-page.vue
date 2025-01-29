555f688a38c21c4bac6be7810410348357b14f43
Dylan K <50585244+dylankainth@users.noreply.github.com>
2025-01-29 11:21:33 +0000

    added homers page

---
+++ pages/homers-page.vue
@@ -0,0 +1,11 @@
+<template>
+
+    <iframe src="https://homers-webpage.vercel.app/" class="w-100 h-screen" scrolling="no"
+        style="height:100vh; overflow:hidden;" />
+</template>
+
+<script>
+export default {
+    layout: 'blank'
+}
+</script>
